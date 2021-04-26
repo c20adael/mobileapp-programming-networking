ce0686483d48109c6720dbe7d0054b1f16fcba69
c20adael <c20adael@student.his.se>
2021-04-26 12:54:16 +0200

    lagt till internet access. Fört in Json task och även lagt in den del som parsar Json datan som kommer från sidan.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,36 +2,125 @@ package com.example.networking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private ArrayList<Mountain> mountainArrayList=new ArrayList<>();
-
+    public ArrayAdapter<Mountain> adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this,R.layout.test,R.id.textview2,mountainArrayList);
+        adapter=new ArrayAdapter<Mountain>(this,R.layout.test,R.id.textview2,mountainArrayList);
         ListView hej=findViewById(R.id.ListView);
         hej.setAdapter(adapter);
-        mountainArrayList.add(new Mountain("Matterhorn","Alps",4478));
-        mountainArrayList.add(new Mountain("Mont Blanc","Alps",4808));
-        mountainArrayList.add(new Mountain("Denali","Alaska",6190));
 
+        Button button = findViewById(R.id.button78);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+
+
+            }
+        });
 
     }
 
 
 
-}
 
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            try{
+                mountainArrayList.clear();
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                String name = jsonObject.getString("name");
+                String location = jsonObject.getString("location");
+                int height = jsonObject.getInt("size");
+                Mountain mountain = new Mountain(name,location,height);
+                mountainArrayList.add(mountain);
+                }
+                adapter.notifyDataSetChanged();
+
+
+            } catch (JSONException e) {
+                Log.d("JSONbajs", json+e);
+            }
+
+
+
+        }
+    }
+}
 
 
