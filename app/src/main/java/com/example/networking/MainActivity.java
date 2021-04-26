a691814b89e2b25e24166efef57e8b77036d70ae
c20adael <c20adael@student.his.se>
2021-04-26 11:38:35 +0200

    Det har lagts till en Array adapter som kopplar ihop textview xml filen med dens skapta listan som heter mountain

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ArrayList<Mountain> mountainArrayList=new ArrayList<>();
+    ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this,R.layout.test,R.id.textview2,mountainArrayList);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
